a90dfa80b9ad302ee69f8c14fe8e3bcdb6748cfe
木花开耶姬 <konohanawa@163.com>
2026-09-09 10:06:02 +0800

    发布 Source/VRMFaceNodes · VRMFaceNodes 1.3.0

---
+++ Source/VRMFaceNodes/VRMFaceNodes.Build.cs
@@ -0,0 +1,10 @@
+using UnrealBuildTool;
+public class VRMFaceNodes : ModuleRules
+{
+    public VRMFaceNodes(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine", "AnimGraphRuntime", "LiveLinkInterface", "LiveLinkAnimationCore" });
+        PrivateDependencyModuleNames.AddRange(new[] { "Slate", "SlateCore", "InputCore", "Json", "JsonUtilities" });
+    }
+}
