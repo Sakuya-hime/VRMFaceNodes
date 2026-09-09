20d120c4868033d8b56a15d1d0f57fc2ecfcd824
木花开耶姬 <konohanawa@163.com>
2026-09-09 10:07:02 +0800

    发布 Source/VRMFaceNodesEditor · VRMFaceNodes 1.3.0

---
+++ Source/VRMFaceNodesEditor/VRMFaceNodesEditor.Build.cs
@@ -0,0 +1,11 @@
+using UnrealBuildTool;
+public class VRMFaceNodesEditor : ModuleRules
+{
+    public VRMFaceNodesEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine", "AnimGraph", "BlueprintGraph", "VRMFaceNodes" });
+        PrivateDependencyModuleNames.AddRange(new[] { "LiveLinkGraphNode", "LiveLinkAnimationCore", "AnimGraphRuntime", "StaticMeshDescription" });
+        PrivateDependencyModuleNames.AddRange(new[] { "UnrealEd", "Kismet", "KismetCompiler", "AssetRegistry", "AssetTools", "Projects", "Json", "JsonUtilities", "MeshDescription", "SkeletalMeshDescription", "Blutility", "IKRig", "IKRigEditor", "Slate", "SlateCore", "InputCore", "ToolMenus", "PropertyEditor", "ContentBrowser" });
+    }
+}
